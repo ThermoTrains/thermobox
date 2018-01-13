70f6785b0afee66dc78e57379cc885d41a996298
Sebastian Häni <haeni.sebastian@gmail.com>
2018-01-13 11:44:11 +0100

    code style

--- src/main/csharp/Common/src/Util/FileUtil.cs
+++ src/main/csharp/Common/src/Util/FileUtil.cs
@@ -30,6 +30,15 @@ namespace SebastianHaeni.ThermoBox.Common.Util
         }
 
         /// <summary>
+        /// Generates a timestamp that can be used as a filename.
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateTimestampFilename()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd@HH-mm-ss");
+        }
+
+        /// <summary>
         /// Moves a file into the Windows Recycle bin. Use the settings on the recycle bin
         /// to define the size limit of the recycle bin folder.
         /// </summary>
@@ -49,21 +58,12 @@ namespace SebastianHaeni.ThermoBox.Common.Util
         }
 
         /// <summary>
-        /// Generates a timestamp that can be used as a filename.
-        /// </summary>
-        /// <returns></returns>
-        public static string GenerateTimestampFilename()
-        {
-            return DateTime.Now.ToString("yyyy-MM-dd@HH-mm-ss");
-        }
-
-        /// <summary>
         /// Ugh! This runs script tasks on MTA threads but Shell32 only wants to 
         /// run on STA thread. So start a new STA thread to call new Shell, block 
         /// till it's done, then return. 
-        /// We use Shell32 since .net doesn't have Recycling Bin interface and we 
-        /// prefer not to ship other dlls as they normally need to be deployed to 
-        /// the GAC. So this is easiest, although not very pretty.
+        /// We use Shell32 since .NET doesn't have Recycling Bin interface and we 
+        /// prefer not to ship other dlls. So this is easiest, although not very 
+        /// pretty.
         /// </summary>
         private static void MoveToRecycleBinInternal(object filepath)
         {
